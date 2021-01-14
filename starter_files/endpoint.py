1887c0a35c58c2880904a668c311a4de1e9b12e1
sreenithi <19sreenithi98@gmail.com>
2021-01-14 15:29:02 +0000

    updated files

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://50bd9bfc-3a3d-4d6c-b43f-902ccf939e36.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = '252tI9LicdT31vE3X66p0nNfB6vfNC2B'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
